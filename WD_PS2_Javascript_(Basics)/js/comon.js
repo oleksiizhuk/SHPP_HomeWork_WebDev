028129396d1e16e91fb53457b8883b4546d82188
oleksii_zhuk <oleksiizhuk.att@gmail.com>
2018-03-17 20:38:47 +0200

    js

---
+++ WD_PS2_Javascript_(Basics)/js/comon.js
@@ -0,0 +1,10 @@
+function first_task() {
+	var firstNumber;
+	var secondNumber;
+	alert (firstNumber);
+	var result = 0;
+	for(var i = firstNumber; firstNumber < secondNumber;i++){
+		result = result + firstNumber
+	}
+	document.getElementById('ceazerRessult').value = result;
+}
