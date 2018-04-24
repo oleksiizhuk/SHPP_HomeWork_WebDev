0077d6b2f8d33d2282eb864cc82129841511b5cc
oleksii_zhuk <oleksiizhuk.att@gmail.com>
2018-04-24 10:58:17 +0300

    finish JQ

--- WD_PS3_jQuery/leading/js/comon.js
+++ WD_PS3_jQuery/leading/js/comon.js
@@ -1,8 +1,26 @@
 $(document).ready(function(){
+
 	$(".main").on("click","a", function (event) {
 		event.preventDefault();
 		var id  = $(this).attr('href'),
 			top = $(id).offset().top;
-		$('body,html').animate({scrollTop: top}, 1500);
+		$('body,html').animate({scrollTop: top}, 1000);
 	});
+
+
+	$(function() { 
+		$(window).scroll(function() {
+ 		if( $(this).scrollTop() !=  0) {
+	 		$('#go-up').fadeIn();
+ 		} else {
+			$('#go-up').fadeOut();
+ 		}
+	});
+ 
+	$('#go-up').click(function() {
+ 		$('body,html').animate({scrollTop:0},1000);
+ 	});
+ 	
+ });
+
 });
