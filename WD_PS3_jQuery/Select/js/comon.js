53aff248ea7144c4970dda346d479591d2895c56
Oleksii Zhuk <oleksiizhuk.att@gmail.com>
2018-05-15 08:05:56 +0300

    jq

--- WD_PS3_jQuery/Select/js/comon.js
+++ WD_PS3_jQuery/Select/js/comon.js
@@ -1,41 +1,30 @@
 
 $(document).ready(function(){
-	let i = 1;
-	let icontop = "&#9207;";
-	let iconBotton = "&#9206;";
+	const icontop = "&#9207;";
+	const iconBotton = "&#9206;";
 
 	let massName = ["Jenny Hess", "Elliot Fu","Stevie Feliciano","Christian","Matt Damon"];
 
-	$("#selectFreand").click(function(){ // открыть закрыть список 
-		i++;
-		if(i % 2 == 0) {
-			$(".ul").show();
-			 $("#iconHouseFirst").hide();
-			 $("#iconHouseSecond").show();
-		}
-		else{
-			$(".ul").hide();
-			$("#iconHouseFirst").show();
-			 $("#iconHouseSecond").hide();
-		}
+	$(".btn-slide").click(function(){
+	  $("#panel").slideToggle("slow");
+	  $(this).toggleClass("active");
 	});
 
-	$(".ul").on("click", "li", function(){ //  заменить ник 
-		let name = $(this).text();
-		alert(name);
+	$(".ul").on("click", "li", function() { //  заменить ник 
+		const name = $(this).text();
 		$(this).hide();
-		$(".ul").hide();
+		$("#panel").slideToggle("slow");
 		$("#selectFreand").text(name);	
 		i++;
 	});
 
-    $("#buttonAddFreand").click(function(){ // добавить  друга
-    	let addFriend = $(".addFreand").val();
+    $("#buttonAddFreand").click(function() { // добавить  друга
+    	const addFriend = $(".addFreand").val();
 		if (addFriend === '') {
 			alert("You must write something!");
 			return;
 		}
-    	let friend =`<li><img src='image/иконка.png'><span>${addFriend}</span></li>`; 
+    	const friend =`<li><img src='image/иконка.png'><span>${addFriend}</span></li>`; 
     	alert(friend);
     	 $("ul").append(friend);
 
