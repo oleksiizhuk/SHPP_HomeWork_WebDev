5f933d7c4d193fbf7f7576608eb7b89a33839a1d
oleksii_zhuk <oleksiizhuk.att@gmail.com>
2018-05-21 15:20:48 +0300

    fix JQ

--- WD_PS3_jQuery/Select/js/comon.js
+++ WD_PS3_jQuery/Select/js/comon.js
@@ -1,33 +1,36 @@
+$(document).ready(function () {
+  const massName = ['Jenny Hess', 'Elliot Fu', 'Stevie Feliciano', 'Christian', 'Matt Damon'];
+  let listName;
+  for (let i = 0; i < massName.length - 1; i++) {
+    listName = `<li><img src='image/images.png'><span>${massName[i]}</span></li>`;
+    $('ul').append(listName);
+  }
 
-$(document).ready(function(){
-	const icontop = "&#9207;";
-	const iconBotton = "&#9206;";
+  $('.btn-slide').click(function () {
+    $('#panel').slideToggle('slow');
+    $(this).toggleClass('active');
+  });
 
-	let massName = ["Jenny Hess", "Elliot Fu","Stevie Feliciano","Christian","Matt Damon"];
+  $('.ul').on('click', 'li', function () {  //  заменить ник
+    const name = $(this).text();
+    $(this).remove();
+    $('#panel').slideToggle('slow');
+    $('#selectFreand').text(name);
+  });
 
-	$(".btn-slide").click(function(){
-	  $("#panel").slideToggle("slow");
-	  $(this).toggleClass("active");
-	});
+  $('#buttonAddFreand').click(function () { // добавить  друга
+    let addFriends = $('.addFreand').val();
+    if (addFriends === '') {
+      alert('You must write something!');
+      return;
+    }
 
-	$(".ul").on("click", "li", function() { //  заменить ник 
-		const name = $(this).text();
-		$(this).hide();
-		$("#panel").slideToggle("slow");
-		$("#selectFreand").text(name);	
-		i++;
-	});
+    if (addFriend.length > 16) {
+      addFriend = addFriend.substring(0, 16);
+    }
 
-    $("#buttonAddFreand").click(function() { // добавить  друга
-    	const addFriend = $(".addFreand").val();
-		if (addFriend === '') {
-			alert("You must write something!");
-			return;
-		}
-    	const friend =`<li><img src='image/иконка.png'><span>${addFriend}</span></li>`; 
-    	alert(friend);
-    	 $("ul").append(friend);
+    const friend = `<li><img src='image/images.png'><span>${addFriend}</span></li>`;
+    $('ul').append(friend);
+  });
 
-    })
- 
 });
--- WD_PS3_jQuery/leading/js/comon.js
+++ WD_PS3_jQuery/leading/js/comon.js
@@ -1,26 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $('.main').on('click', 'a', function (event) { //  по клике на сылку смешаеться
+    event.preventDefault();
+    const id = $(this).attr('href'),
+     top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1000);
+  });
 
-	$(".main").on("click","a", function (event) {
-		event.preventDefault();
-		var id  = $(this).attr('href'),
-			top = $(id).offset().top;
-		$('body,html').animate({scrollTop: top}, 1000);
-	});
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#go-up').fadeIn();
+    } else {
+      $('#go-up').fadeOut();
+    }
+  });
 
-
-	$(function() { 
-		$(window).scroll(function() {
- 		if( $(this).scrollTop() !=  0) {
-	 		$('#go-up').fadeIn();
- 		} else {
-			$('#go-up').fadeOut();
- 		}
-	});
- 
-	$('#go-up').click(function() {
- 		$('body,html').animate({scrollTop:0},1000);
- 	});
- 	
- });
+  $('#go-up').click(function () {
+      $('body,html').animate({
+        scrollTop: 0,
+      }, 800);
+      return false;
+    });
 
 });
