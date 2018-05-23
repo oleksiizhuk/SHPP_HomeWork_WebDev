7580195ac484b6773d11dfbf6e8c8ed48a55ae98
oleksii_zhuk <oleksiizhuk.att@gmail.com>
2018-05-23 13:28:32 +0300

    jq

--- WD_PS3_jQuery/Select/js/comon.js
+++ WD_PS3_jQuery/Select/js/comon.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
   const massName = ['Jenny Hess', 'Elliot Fu', 'Stevie Feliciano', 'Christian', 'Matt Damon'];
   let listName;
+  let ul =  $('ul');
   for (let i = 0; i < massName.length - 1; i++) {
     listName = `<li><img src='image/images.png'><span>${massName[i]}</span></li>`;
-    $('ul').append(listName);
+    ul.append(listName);
   }
 
   $('.btn-slide').click(function () {
@@ -19,12 +20,14 @@ $(document).ready(function () {
   });
 
   $('#buttonAddFreand').click(function () { // добавить  друга
-    let addFriends = $('.addFreand').val();
-    if (addFriends === '') {
-      alert('You must write something!');
+    let addFriend = $('.addFreand').val();
+    let errBlock = $('.alertMessege p');
+    if (addFriend === '') {
+      errBlock.text('You must write something!');
       return;
     }
 
+    errBlock.text('');
     if (addFriend.length > 16) {
       addFriend = addFriend.substring(0, 16);
     }
