b4a4604885b397a33a11de986e70ea0894c35bbb
223132qq <oleksiizhuk.att@gmail.com>
2018-06-08 16:11:54 +0200

    add icon and fix animation in select

--- WD_PS3_jQuery/Select/js/comon.js
+++ WD_PS3_jQuery/Select/js/comon.js
@@ -1,39 +1,43 @@
 $(document).ready(function () {
-  const massName = ['Jenny Hess', 'Elliot Fu', 'Stevie Feliciano', 'Christian', 'Matt Damon'];
-  let listName;
-  let ul =  $('ul');
-  for (let i = 0; i < massName.length - 1; i++) {
-    listName = `<li><img src='image/images.png'><span>${massName[i]}</span></li>`;
-    ul.append(listName);
-  }
+	const massName = ['Jenny Hess', 'Elliot Fu', 'Stevie Feliciano', 'Christian', 'Matt Damon'];
+	let listName;
+	const onClickSelect = $('#selectFreand');
+	let ul =  $('ul');
+	for (let i = 0; i < massName.length - 1; i++) {
+		listName = `<li><img src='image/${i+1}.jpg'><span>${massName[i]}</span></li>`;
+		ul.append(listName);
+	}
 
-  $('#selectFreand').click(function () { // slide booton
-    $('#panel').slideToggle('slow');
-    $(this).toggleClass('active');
-  });
+			onClickSelect.on('click', function () { // slide booton
+				if( $('#panel').is(':animated') ){
+					return;
+				} 
+				$('#panel').slideToggle('slow');
+			});
+	 
 
-  $('.ul').on('click', 'li', function () {  //  заменить ник
-    const name = $(this).text();
+	$('.ul').on('click', 'li', function () {  //  заменить ник
+		const name = $(this).text();
  
-    $('#panel').slideToggle('slow');
-    $('#selectFreand').text(name);
-  });
+		$('#panel').slideToggle('slow');
+		$('#selectFreand').text(name);
+	});
 
-  $('#buttonAddFreand').click(function () { // добавить  друга
-    let addFriend = $('.addFreand').val();
-    let errBlock = $('.alertMessege').find('p');
-    if (!addFriend.length) {
-      errBlock.text('You must write something!');
-      return;
-    }
+	$('#buttonAddFreand').click(function () { // добавить  друга
+		let addFriend = $('.addFreand').val();
+		let errBlock = $('.alertMessege').find('p');
+		if (!addFriend.length) {
+			errBlock.text('You must write something!');
+			return;
+		}
 
-    errBlock.text('');
-    if (addFriend.length > 16) {
-      addFriend = addFriend.substring(0, 16);
-    }
-
-    const friend = `<li><img src='image/images.png'><span>${addFriend}</span></li>`;
-    $('ul').append(friend);
-  });
+		errBlock.text('');
+		if (addFriend.length > 16) {
+			addFriend = addFriend.substring(0, 16);
+		}
+		let rand = Math.floor(Math.random() * 6);
+		const friend = `<li><img src='image/${rand}.jpg'><span>${addFriend}</span></li>`;
+		$('ul').append(friend);
+	});
 
 });
