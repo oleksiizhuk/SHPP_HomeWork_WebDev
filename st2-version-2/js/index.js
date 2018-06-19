70ab6585609be431cb289cb8c71e3ba8ebe9b9ea
Oleksii Zhuk <oleksiizhuk.att@gmail.com>
2018-06-20 00:00:13 +0300

    add media

--- st2-version-2/js/index.js
+++ st2-version-2/js/index.js
@@ -1,3 +1,10 @@
 function sandwich(change) {
     change.classList.toggle("change");
+
+    if ($(".show-for-surprize").is(":visible")) {
+    	$(".show-for-surprize").fadeOut(600);
+    } else {
+    	$(".show-for-surprize").fadeIn(600);
+    }
+   
 }
