5f9d051717b645769706834533604bf83ab925dc
223132qq <oleksiizhuk.att@gmail.com>
2018-06-20 17:40:31 +0200

    add JQ

--- st2-version-2/js/index.js
+++ st2-version-2/js/index.js
@@ -1,10 +1,21 @@
-function sandwich(change) {
-    change.classList.toggle("change");
+$(document).ready(function () {
 
-    if ($(".show-for-surprize").is(":visible")) {
-    	$(".show-for-surprize").fadeOut(600);
-    } else {
-    	$(".show-for-surprize").fadeIn(600);
-    }
-   
-}
+	$(".sandwich").click(function() {
+		$(".sandwich").toggleClass("change");
+
+		if ($(".show-for-surprize").is(":visible")) {
+			$(".show-for-surprize").fadeOut(600);
+			$(".sandwich").css("position", "absolute");
+		} else {
+			$(".show-for-surprize").fadeIn(600);
+			$(".sandwich").css("position", "fixed");
+		}
+	});
+
+	$('.button-go-up').click(function () {
+		$('body,html').animate({
+			scrollTop: 0,
+		}, 800);
+		return false;
+	});
+});
