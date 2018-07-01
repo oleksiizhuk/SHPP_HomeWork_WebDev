1bc0cf81be5d575d71c097d5e168e5de423177b4
Oleksii Zhuk <oleksiizhuk.att@gmail.com>
2018-07-02 01:03:49 +0300

    fix scroll,rendered variables, replace name variables,add more const

--- WD_PS3_jQuery/Select/js/comon.js
+++ WD_PS3_jQuery/Select/js/comon.js
@@ -1,31 +1,40 @@
 $(document).ready(function () {
 	const massName = ['Jenny Hess', 'Elliot Fu', 'Stevie Feliciano', 'Christian', 'Matt Damon'];
 	let listName;
-	const onClickSelect = $('#selectFreand');
-	let ul =  $('ul');
+	const panel = $('#panel');
+	const selectedFriend = $('#selectedFriend');
+	const ul =  $('ul');
 	for (let i = 0; i < massName.length - 1; i++) {
 		listName = `<li><img src='image/${i+1}.jpg'><span>${massName[i]}</span></li>`;
 		ul.append(listName);
 	}
 
-			onClickSelect.on('click', function () { // slide booton
-				if( $('#panel').is(':animated') ){
-					return;
-				} 
-				$('#panel').slideToggle('slow');
-			});
+	selectedFriend.on('click', function () { // slide booton
+		if( panel.is(':animated') ){
+			return;
+		} 
+		panel.slideToggle('slow');
+	});
+  /*если клик вне блока, сровачивает его, если он развернут*/
+	$(document).on('click', function(e) { 
+		if (!$(e.target).closest(".list").length) {
+			panel.hide('slow');
+		}
+		e.stopPropagation();
+	});
+	
 	 
 
-	$('.ul').on('click', 'li', function () {  //  заменить ник
+	ul.on('click', 'li', function () {  //  заменить ник
 		const name = $(this).text();
  
-		$('#panel').slideToggle('slow');
-		$('#selectFreand').text(name);
+		panel.slideToggle('slow');
+		selectedFriend.text(name);
 	});
 
 	$('#buttonAddFreand').click(function () { // добавить  друга
 		let addFriend = $('.addFreand').val();
-		let errBlock = $('.alertMessege').find('p');
+		const errBlock = $('.alertMessege').find('p');
 		if (!addFriend.length) {
 			errBlock.text('You must write something!');
 			return;
@@ -35,9 +44,9 @@ $(document).ready(function () {
 		if (addFriend.length > 16) {
 			addFriend = addFriend.substring(0, 16);
 		}
-		let rand = Math.floor(Math.random() * 6);
+		const rand = Math.floor(Math.random() * 6);
 		const friend = `<li><img src='image/${rand}.jpg'><span>${addFriend}</span></li>`;
-		$('ul').append(friend);
+		ul.append(friend);
 	});
 
 });
--- WD_PS3_jQuery/leading/js/comon.js
+++ WD_PS3_jQuery/leading/js/comon.js
@@ -1,24 +1,34 @@
 $(document).ready(function () {
-  $('.main').on('click', 'a', function (event) { //  по клике на сылку смешаеться
-    event.preventDefault();
-    const id = $(this).attr('href'),
-     top = $(id).offset().top;
-    $('body,html').animate({ scrollTop: top }, 1000);
-  });
+	const animationSpeed = 1000;
+	const site = $("html, body");
+	const goUp = $('#go-up');
 
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      $('#go-up').fadeIn();
-    } else {
-      $('#go-up').fadeOut();
-    }
-  });
+	$('.main').on('click', 'a', function (event) { //  по клике на сылку смешаеться
+		event.preventDefault();
+		const id = $(this).attr('href'),
+		top = $(id).offset().top;
+		$('body,html').animate({ scrollTop: top }, animationSpeed);
 
-  $('#go-up').click(function () {
-      $('body,html').animate({
-        scrollTop: 0,
-      }, 800);
-      return false;
-    });
+		site.on("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function(){ // остановка анимации при клике на страницу
+			site.stop();
+		});
+
+	});
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			goUp.fadeIn();
+		} else {
+			goUp.fadeOut();
+		}
+	});
+
+		goUp.click(function () {
+			$('body,html').animate({
+				scrollTop: 0,
+			}, animationSpeed);
+			return false;
+		});
 
 });
+
