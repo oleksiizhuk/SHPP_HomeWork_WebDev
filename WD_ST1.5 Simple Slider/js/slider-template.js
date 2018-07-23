dcf54ad37f7615f82457dd5a656f9145122f009b
Oleksii Zhuk <oleksiizhuk.att@gmail.com>
2018-07-23 20:09:02 +0300

    add two version

--- WD_ST1.5 Simple Slider/js/slider-template.js
+++ WD_ST1.5 Simple Slider/js/slider-template.js
@@ -12,89 +12,94 @@ const IMAGES = [
 ];
 
 $(document).ready(function () {
-	let img ;
+	let img;
 	let ul = $('ul');
 	for (var i = 0; i <= IMAGES.length - 1; i++) {
 		if (i === 0) {
-			img = `<li class="img active"><img src='${API_URL}${SMALL_SIZE}/${SMALL_SIZE}/${IMAGES[i]}' </li>`;
+			img = `<li class="img active"><img src='${API_URL}${SMALL_SIZE}/${IMAGES[i]}'></li>`;
 			ul.append(img);
 		} else {
-			img = `<li class="img"><img src='${API_URL}${SMALL_SIZE}/${SMALL_SIZE}/${IMAGES[i]}' ></li>`;
+			img = `<li class="img"><img src='${API_URL}${SMALL_SIZE}/${IMAGES[i]}'></li>`;
 			ul.append(img);
 		}
 	}
 });
-
+$('.slider-current').click(function(){
+	nextSlide();
+});
 
 document.addEventListener("keyup", function(e){
 	let key = e.key + e.location;
 	console.log(key);
 	if( key == "ArrowLeft0" ){
-		/*slide("0");*/
-		slideLeft();
+		/*prevSlide();*/
+		prevNextSlide("0");
 	}
 	if(key == "ArrowRight0"){
-		/*slide("1");*/
-		slideRight();
+		/*nextSlide();*/
+		prevNextSlide("1");
 	}
 });
 
-function slide (click) { 
-	if(click == 1) {
-		let currImage = $('.img.active');
-		let currImageIndex = $('.img.active').index();
-		console.log('currImageIndex - ' + currImageIndex);
-		if(currImageIndex == IMAGES.length - 1){
-			currImageIndex = -1;
+function nextSlide() {
+	let currImage = $('.img.active');
+	let currImageIndex = $('.img.active').index();
+	console.log('currImageIndex - ' + currImageIndex);
+	if(currImageIndex == IMAGES.length - 1){
+		currImageIndex = -1;
+	}
+	let nextImageIndex = currImageIndex + 1;
+	let photo = $('.slider-current img');
+	const src = `https://picsum.photos/600/400/${IMAGES[nextImageIndex]}`;
+	photo.attr('src', src);
+	let nextImage = $('.img').eq(nextImageIndex);
+	currImage.removeClass('active');
+	nextImage.addClass('active');
+}
+
+function prevSlide() {
+	let currImage = $('.img.active');
+	let currImageIndex = $('.img.active').index();
+	console.log('currImageIndex - ' + currImageIndex);
+	if(currImageIndex == 0) {
+		currImageIndex = IMAGES.length;
+	}
+	let nextImageIndex = currImageIndex - 1;
+	let photo = $('.slider-current img');
+	const src = `https://picsum.photos/600/400/${IMAGES[nextImageIndex]}`;
+	photo.attr('src', src);
+	let nextImage = $('.img').eq(nextImageIndex);
+	currImage.removeClass('active');
+	nextImage.addClass('active');
+}
+function prevNextSlide (prevOrNext) {
+	console.log('prevOrNext - ' + prevOrNext);
+	let currImage = $('.img.active');
+	let currImageIndex = $('.img.active').index();
+	console.log('currImageIndex - ' + currImageIndex);
+
+	let nextImageIndex = checkIndeximage(currImageIndex, prevOrNext);
+	let photo = $('.slider-current img');
+	const src = `https://picsum.photos/600/400/${IMAGES[nextImageIndex]}`;
+	photo.attr('src', src);
+	let nextImage = $('.img').eq(nextImageIndex);
+	currImage.removeClass('active');
+	nextImage.addClass('active');
+}
+
+function checkIndeximage(index, prevOrNext) {
+	console.log('index - ' + index +' prevOrNext - ' + prevOrNext);
+
+	if (prevOrNext == 0) {
+		if(index == 0) {
+			return IMAGES.length - 1;
 		}
-
-		let nextImageIndex = currImageIndex + 1;
-
-		let photo = $('.slider-current img');
-		const src = `https://picsum.photos/600/400/${IMAGES[nextImageIndex]}`;
-		photo.attr('src', src);
-
-		let nextImage = $('.img').eq(nextImageIndex);
-		currImage.removeClass('active');
-		nextImage.addClass('active');
+		return index - 1;
 	} else {
-		console.log('work');
-	}
-
-	function slideRight() {
-		let currImage = $('.img.active');
-		let currImageIndex = $('.img.active').index();
-		console.log('currImageIndex - ' + currImageIndex);
-		if(currImageIndex == IMAGES.length - 1){
-			currImageIndex = -1;
+		if(index == IMAGES.length - 1){
+			return 0;
 		}
-		let nextImageIndex = currImageIndex + 1;
-		let photo = $('.slider-current img');
-		const src = `https://picsum.photos/600/400/${IMAGES[nextImageIndex]}`;
-		photo.attr('src', src);
-
-		let nextImage = $('.img').eq(nextImageIndex);
-		currImage.removeClass('active');
-		nextImage.addClass('active');
-	}
-
-	function slideLeft() {
-		let currImage = $('.img.active');
-		let currImageIndex = $('.img.active').index();
-		console.log('currImageIndex - ' + currImageIndex);
-		if(currImageIndex == IMAGES.length - 1) {
-			currImageIndex = 7;
-		}
-
-		let nextImageIndex = currImageIndex - 1;
-
-		let photo = $('.slider-current img');
-		const src = `https://picsum.photos/600/400/${IMAGES[nextImageIndex]}`;
-		photo.attr('src', src);
-
-		let nextImage = $('.img').eq(nextImageIndex);
-		currImage.removeClass('active');
-		nextImage.addClass('active');
+		return index + 1;
 	}
 
 }
