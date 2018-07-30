b7eee91494cf6975fde7682d15485e48d35574a2
Oleksii Zhuk <oleksiizhuk.att@gmail.com>
2018-07-30 17:54:03 +0300

    refactoring done

--- WD_ST1.5_Simple_Slider/js/slider-template.js
+++ WD_ST1.5_Simple_Slider/js/slider-template.js
@@ -12,61 +12,60 @@ const IMAGES = [
 ];
 
 $(function() {
-	let image = "";
 	const sliderPreviews = $('.slider-previews');
+	let thumbnails = '';
 	for (let i = 0; i <= IMAGES.length - 1; i++) {
-		if (i === 0) {
-			image += `<li class="img current"><img src='${API_URL}${SMALL_SIZE}/${IMAGES[i]}'></li>`;
+		thumbnails += `
+			<li class="${i ? 'img' : 'img current'}">
+				<img src="${API_URL + SMALL_SIZE + IMAGES[i]}">
+			</li>
+		`;
+	}
+	sliderPreviews.append(thumbnails);
+
+	$('.slider-current').on('click', 'img', function() {
+		prevNextSlide("1");
+	});
+
+	$('.slider-previews').on('click', 'li', function() {  
+		const nextSlide = $( this ).index();
+		prevNextSlide(false, nextSlide);
+	});
+
+	$(document).keydown(function(event) {
+		const key = event.keyCode;
+		if (key === 37) { 
+			prevNextSlide("0");
+		}
+		if (key === 39){
+			prevNextSlide("1");
+		}
+	});
+
+	function prevNextSlide(prevOrNext, nextSlide) {
+		if (prevOrNext === false) {
+			changePicture(nextSlide);
 		} else {
-			image += `<li class="img"><img src='${API_URL}${SMALL_SIZE}/${IMAGES[i]}'></li>`;
+			const currImageIndex = $('.current').index();
+			const nextImageIndex = checkIndexImage(currImageIndex, prevOrNext);
+			changePicture(nextImageIndex);
 		}
 	}
-	sliderPreviews.append(image);
-});
 
-$('.slider-current').on('click', 'img', function() {
-	prevNextSlide("1");
-});
-
-$('.slider-previews').on('click', 'li', function() {  
-	const nextSlide = $( this ).index();
-	prevNextSlide(false, nextSlide);
-});
-
-$(document).keydown(function(event) {
-	const key = event.keyCode;
-	if (key== 37) { 
-		prevNextSlide("0");
+	function changePicture(nextImageIndex) {
+		const photo = $('.slider-current img');
+		const src = `${API_URL}${BIG_SIZE}/${IMAGES[nextImageIndex]}`;
+		photo.attr('src', src);
+		const nextImage = $('.img').eq(nextImageIndex);
+		const currImage = $('.current');
+		currImage.removeClass('current');
+		nextImage.addClass('current');
 	}
-	if (key == 39){
-		prevNextSlide("1");
+
+	function checkIndexImage(index, prevOrNext) {
+		if ( prevOrNext === "0") {
+			return index === 0 ? IMAGES.length-1 : index-1;
+		}
+		return index === IMAGES.length - 1 ?  0 : index + 1;
 	}
 });
-
-function prevNextSlide(prevOrNext,nextSlide) {
-	if (prevOrNext === false) {
-		changePicture(nextSlide);
-	} else {
-		const currImageIndex = $('.current').index();
-		const nextImageIndex = checkIndexImage(currImageIndex, prevOrNext);
-		changePicture(nextImageIndex);
-	}
-}
-
-function changePicture(nextImageIndex) {
-	const photo = $('.slider-current img');
-	const src = `${API_URL}${BIG_SIZE}/${IMAGES[nextImageIndex]}`;
-	photo.attr('src', src);
-	const nextImage = $('.img').eq(nextImageIndex);
-	const currImage = $('.current');
-	currImage.removeClass('current');
-	nextImage.addClass('current');
-}
-
-function checkIndexImage(index, prevOrNext) {
-	if (prevOrNext == 0) {
-		return index === 0 ? IMAGES.length-1 : index-1;
-	}
-	return index == IMAGES.length - 1 ?  0 : index + 1;
-}
-
