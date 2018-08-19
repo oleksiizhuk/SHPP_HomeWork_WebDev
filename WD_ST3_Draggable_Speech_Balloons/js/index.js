9286cea90c3ca5e7e6193bfd7cf15c51c516168a
Oleksii Zhuk <oleksiizhuk.att@gmail.com>
2018-08-19 23:33:52 +0300

    fix js

--- WD_ST3_Draggable_Speech_Balloons/js/index.js
+++ WD_ST3_Draggable_Speech_Balloons/js/index.js
@@ -1,29 +1,47 @@
 $(function () {
-	$('#draggable').draggable();
+	$('#draggable').draggable({snap:true});
 	const main = $('#main');
-	const draggable = $('#draggable');
-
-	main.on('click', function() {
-
-	});
+	const draggable = $('.draggable');
+	const site = $("html, body");
+	const html = $("html");
+	const body = $("body");
 
 	main.dblclick(function(event) {
-		const div = $('<div id="draggable" class="ui-widget-content">Drag me around<input type="text"></div>')
+		/*const div = $('<div id="draggable" class="ui-widget-content">Drag me around<input type="text"></div>')
 		.appendTo($(this))
 		.css({
-                "left": event.pageX + 'px',
-                "top": event.pageY + 'px'
-            })
+				"left": event.pageX + 'px',
+				"top": event.pageY + 'px'
+			})
 		.draggable();
+		console.log(this);*/	
+	});
+	$(window).resize(function(event){
+		$(".mainResize").text("Width: "+ window.innerWidth + ", height" +window.innerHeight);
+	});
+
+	main.on("dblclick", function(ev) {
+		drag.createdraggable(event.pageX, event.pageY);
+	});
+
+	body.on("dblclick", '#draggable', function(event) {
+		console.log(this);
+		console.log(event.pageX);
+		console.log(event.pageY);
+		console.log(event.type);
+
 	});
 	
-
-	$('#main #draggable').dblclick(function(event) {
-		alert("x - " + event.pageX);
-		alert("y - " + event.pageY);
-	});
-	const class ={
-		
-	}
-
 });
+const drag ={
+	createdraggable: function (evX, evY) {
+		const div = $('<div id="draggable" class="ui-widget-content">Drag me</div>')
+		.appendTo("body")
+		.css({
+                "left": evX + 'px',
+                "top": evY + 'px'
+            })
+		.bind()
+		.draggable({snap:true});
+	},
+}
