ee43f4b6741990b79a1a92ed3e8d3a57d675a53b
223132qq <oleksiizhuk.att@gmail.com>
2018-08-23 13:30:48 +0200

    add comment in block

--- WD_ST3_Draggable_Speech_Balloons/js/index.js
+++ WD_ST3_Draggable_Speech_Balloons/js/index.js
@@ -25,22 +25,41 @@ $(function () {
 	});
 
 	body.on("dblclick", '#draggable', function(event) {
-		console.log(this);
+		/*console.log(this);
 		console.log(event.pageX);
 		console.log(event.pageY);
-		console.log(event.type);
+		console.log(event.type);*/
 
+		$(this).children().attr("type", "text").focus();
+		const indexInput = $( this ).index();
+	});
+
+	body.on("focusout", '#draggable', function() {
+		$(this).find('input').attr("type", "hidden");
+		const value = $(this).find('input').val();
+		$(this).find('p').text(value);
+		alert(value);
+	});
+
+	$(document).keyup(function(event) {
+		if(event.which == 13) {
+			alert('You pressed enter!');
+		} 
+		if(event.which == 27) {
+			alert('You pressed Escape');
+		}
+		
 	});
 	
 });
 const drag ={
 	createdraggable: function (evX, evY) {
-		const div = $('<div id="draggable" class="ui-widget-content">Drag me</div>')
+		const div = $('<div id="draggable" class="draggable ui-widget-content btn btn-primary" data-toggle="modal" data-target="#exampleModal"><p>Drag me</p><input type="hidden" class="dragInput"> </div>')
 		.appendTo("body")
 		.css({
-                "left": evX + 'px',
-                "top": evY + 'px'
-            })
+				"left": evX + 'px',
+				"top": evY + 'px'
+			})
 		.bind()
 		.draggable({snap:true});
 	},
