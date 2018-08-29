2c5697f7f17647a533ce102b5413654cde91d365
223132qq <oleksiizhuk.att@gmail.com>
2018-08-29 20:21:40 +0200

    delete garbage

--- WD_ST3_Draggable_Speech_Balloons/js/index.js
+++ WD_ST3_Draggable_Speech_Balloons/js/index.js
@@ -1,57 +1,42 @@
 $(function () {
-	
-	const dragOpts = {
-	height: "110px",
-    width: "120px"
 
-					};
 	$('#draggable').draggable({snap:true, containment: ".main", scroll: false});
 	const main = $('#main');
 	const draggable = $('.draggable');
-	const site = $("html, body");
 	const html = $("html");
 	const body = $("body");
+	const enter = 13;
+	const escape = 27;
 
-	main.dblclick(function(event) {
-		/*const div = $('<div id="draggable" class="ui-widget-content">Drag me around<input type="text"></div>')
-		.appendTo($(this))
+	// Create div on the mouse position
+	main.on("dblclick", function(event) {
+		const div = $('<div class="draggable ui-widget-content btn btn-primary" data-toggle="modal" data-target="#exampleModal"><p>corner</p><input type="hidden" class="dragInput" value=""> </div>')
+		.appendTo("main")
 		.css({
 				"left": event.pageX + 'px',
 				"top": event.pageY + 'px'
 			})
-		.draggable();
-		console.log(this);*/	
-	});
-
-	//  Check resize window
-	$(window).resize( function(event) {
-		$(".mainResize").text("Width: "+ window.innerWidth + ", height" +window.innerHeight);
-	});
-
-	// Create div on the mouse position
-	main.on("dblclick", function(event) {
-		drag.createdraggable(event.pageX, event.pageY);
+		.bind()
+		.draggable({snap:true, containment: ".main", scroll: false});	
 	});
 
 	// show input dbclick
 	main.on("dblclick", '.draggable', function(event) {
-		$(this).children().attr("type", "text").focus().val($(this).text().trim());
-		const indexInput = $( this ).index();
+		$(this).find('input').attr("type", "text").focus().val($(this).text().trim());
 		event.stopPropagation();
 	});
 
 	// 
 	main.on("blur", '.draggable', function(event) {
-		$(this).find('input').attr("type", "hidden");
-		const value = $(this).find('input').val();
-		$(this).find('p').text(value);
+		$(event.target).attr("type", "hidden");
+		const value = $(event.target).val();
+		$(event.target).parent().find('p').text(value);
 		alert("focusout");
 	});
 
 
 	$(document).keyup(function(event) {
-		if(event.which === 13) {	
-			//let EvTarg = ;
+		if (event.which === enter) {	
 			let value = $(event.target).val();
 			value = value.slice(0, 70);
 			alert(value);
@@ -64,32 +49,24 @@ $(function () {
 			if (value.length > 58) {
 				$(this).find('div').css("height", "37%");
 			}
-			$(this).parent().find('input').val(value);
-			$(this).parent().find('p').text(value);
-			$(this).find('p').text(value);
+			$(event.target).val(value);
 			$(event.target).attr("type", "hidden");
+			$(event.target).parent().find('p').text(value);	
+			event.stopPropagation();
 		} 
-
-		if(event.which === 27) {
+		if (event.which === escape) {
 			alert("esc");
+			let value = $(event.target).val();
+			$(event.target).val(value);
+			$(event.target).attr("type", "hidden");
+			event.stopPropagation();
 		}
+
 	});
-	
+
+	//  Check resize window
+	/*$(window).resize( function(event) {
+		$(".mainResize").text("Width: "+ window.innerWidth + ", height" +window.innerHeight);
+	});*/
 });
-
-	//create new dtaggeble
-const drag = {
-	createdraggable: function (evX, evY) {
-		const div = $('<div class="draggable ui-widget-content btn btn-primary" data-toggle="modal" data-target="#exampleModal"><p>corner</p><input type="hidden" class="dragInput" value=""> </div>')
-		.appendTo("main")
-		.css({
-				"left": evX + 'px',
-				"top": evY + 'px'
-			})
-		.bind()
-		.draggable({snap:true, containment: ".main", scroll: false});
-	},
-};
-
-
 
