b9b249a78dcbb975c5d29b0eadc20c3119e320f5
223132qq <oleksiizhuk.att@gmail.com>
2018-09-17 02:53:04 +0200

    add form

--- WD_PS5_AJAX/index.php
+++ WD_PS5_AJAX/index.php
@@ -3,8 +3,37 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<link rel="stylesheet" href="css/index.css">
+	<link rel="stylesheet" href="css/normalize/normalize.css">
 </head>
 <body>
-	
+	<header>
+		<div class="conteiner__header">
+		</div>
+	</header>
+	<main>
+		<div class="main__container">
+			<h1>
+			Easy Chat
+		</h1>
+		<form action="#">
+			<input type="text" placeholder="John Doe">
+			<input type="password" placeholder="•••••">
+		</form>
+		</div>
+		
+	</main>
+	<footer>
+		
+	</footer>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="js/index.js"></script>
 </body>
 </html>
+
+/* background-image: linear-gradient(to right,
+	#63A69F 20%,
+	#F2E1AC 20%, #F2E1AC 40%,
+	#F2836B 40%, #F2836B 60%, 
+	#F2594B 60%, #F2594B 80%, 
+	#CD2C24 80%) */
