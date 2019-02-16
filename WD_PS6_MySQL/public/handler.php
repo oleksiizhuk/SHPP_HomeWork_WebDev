6813d163ddcbb4435838a027c4a037821c3bdd86
Oleksii Zhuk <oleksiizhuk.att@gmail.com>
2019-02-16 16:58:09 +0200

    add autoloader

--- WD_PS6_MySQL/public/handler.php
+++ WD_PS6_MySQL/public/handler.php
@@ -5,20 +5,11 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 }
 session_start();
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-define('APP_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
-
-spl_autoload_register(function ($class) {
-    $file = APP_PATH . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
-    if (isset($file) && file_exists($file)) {
-        require_once $file;
-    }
-});
+require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'autoloader.php';
 
 $instance = App\SingleTonConnectToDB::getInstance();
 $conn = $instance->getConnection();
+
 
 if (isset($_POST['submit'])) {
     $verification = new App\Verification($_POST['login'], $_POST['password'], $conn);
